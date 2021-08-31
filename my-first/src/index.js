4af38c8b6989314dd3645c8630d5feb3f91d32e9
SejunCheon <kjsug1030@naver.com>
2021-08-31 20:07:27 +0900

    Start React

---
+++ my-first/src/index.js
@@ -0,0 +1,28 @@
+class Square extends React.Component{
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square />;
+    }
+}
+
+render() {
+    const status = 'Next player: X';
+
+    return (
+        
+    )
+},
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+
