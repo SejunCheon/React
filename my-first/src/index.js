e67f9d7f8cb65b3cc10a0146d3e6b4e449a7dcfc
SejunCheon <kjsug1030@naver.com>
2021-09-26 23:18:52 +0900

    updata Tic-Tac_Toe

--- my-first/src/index.js
+++ my-first/src/index.js
@@ -11,18 +11,11 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    // 모든 React 컴포넌트 클래스는 생성자를 가질 때 super(props) 호출 구문부터 작성
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      //배열의 start index부터 end index 전까지(end index는 미포함) value값으로 채워주는 함수
-      xIsNext: true,
-    };
-  }
-
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       squares: squares,
@@ -33,18 +26,15 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const status = "Next player : " + (this.state.xIsNext ? "X" : "O");
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -66,14 +56,39 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    // 모든 React 컴포넌트 클래스는 생성자를 가질 때 super(props) 호출 구문부터 작성
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+          //배열의 start index부터 end index 전까지(end index는 미포함) value값으로 채워주는 함수
+        },
+      ],
+      xIsNext: true,
+    };
+  }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = "Winner : " + winner;
+    } else {
+      status = "Next player : " + (this.state.xIsNext ? "X" : "O");
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/*status*/}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
