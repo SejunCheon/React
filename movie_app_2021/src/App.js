2833477072aded6e9673156669754280c8ccd407
SejunCheon <kjsug1030@naver.com>
2021-09-28 19:46:58 +0900

    second time React CloneCoding

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,61 +1,35 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image:
-      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-    // rating: 5.0,
-  },
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
 
-  {
-    id: 2,
-    name: "samgyetang",
-    image:
-      "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-    rating: 4.5,
-  },
+  state = {
+    count: 0,
+  };
 
-  {
-    id: 3,
-    name: "watermelon",
-    image:
-      "https://ww.namu.la/s/39b3e728107195de9f39f45cb873f3a1ab60f2ddb8a7b044784c76d7d19d476c00e588331c7928b610ab49dc429cb3fbbbd6cb6332e046545a16c247beec948b9adb94e21941040c247aac315310191445229df0276c485fc341d3f1cd9af728",
-    rating: 3.2,
-  },
-];
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I Like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("componentRender");
+  }
+  render() {
+    console.log("Im rendering");
+    return (
+      <div>
+        <h1>The number is {this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
