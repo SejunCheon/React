60be6970c7e7f7b9995b6bad7f3de583b27a3d87
SejunCheon <kjsug1030@naver.com>
2021-12-24 11:41:54 +0900

    start react test

---
+++ my-yes/src/App.js
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+      App
+      <div>
+        <Button variant="primary">눌러봐라</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
