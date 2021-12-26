bedb5ce648e5945ef4aaab89247f6793512bd972
SejunCheon <kjsug1030@naver.com>
2021-12-26 23:51:07 +0900

    covid19 page clone_coding start

---
+++ covid19/src/components/Contents.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { Bar, Dought, Line } from "react-chartjs-2";
+import axios from "axios";
+
+const Contents = () => {
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await axios.get(
+        "https://api.covid19api.com/total/dayone/country/kr"
+      );
+      console.log(res);
+      // .then((res) => {
+      //   console.log(res);
+      // })
+      // .catch((err) => {
+      //   console.log(err);
+      // });
+    };
+
+    fetchEvents();
+  });
+
+  return (
+    <section>
+      <h2>국내 코로나 현황</h2>
+      <div className="contents">
+        <div>
+          <Bar
+            data={confirmedData}
+            options={
+              ({
+                title: {
+                  display: true,
+                  text: "누적 환진자 추이",
+                  fontSize: 16,
+                },
+              },
+              { legend: { display: true, position: "bottom" } })
+            }
+          ></Bar>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contents;
